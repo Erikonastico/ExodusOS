0c122d69d15b0d3399151714e83c03b96149a811
Erikonastico <39448705+Erikonastico@users.noreply.github.com>
2020-11-30 21:24:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+//Login//
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('NzgzMDkzMDg0OTM3NTE5MTA0.X8VuRA.xwNgUct-evDrxJ_uHQslulMuzWE');
+
+//Mensage Listening//
+client.on('message', message => {
+    var saved = '';
+    var randomValue;
+    var mensagem = message.content;
+    var mensagemDividida = mensagem.split(" ")
+    console.log(Instruções);
+	if (mensagemDividida[0] === '!roll') {
+        var Instruções = mensagemDividida[1].split("");
+        for (n = 0; n < Instruções.length; ++n) {
+        saved += Instruções[n];
+        }
+        randomValue = Math.floor(Math.random() * saved) + 1;
+        console.log(saved);
+        message.channel.send('d' + mensagemDividida[1] + ' = **[' + randomValue + ']**');
+    }
+});
