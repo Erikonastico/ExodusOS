842534460913e634fbbcd98c6190dc997fc0f47b
Erikonastico <39448705+Erikonastico@users.noreply.github.com>
2020-12-04 11:35:07 -0300

    Update index.js
    Sistema simples de conquistas + parenteses.

--- index.js
+++ index.js
@@ -8,88 +8,123 @@ client.once('ready', () => {
 
 //Mensage Listening//
 client.on('message', message => {
-    //Variable Declaration//
+    
+    //Global Variables//
     var author_id = message.author.id;
     var randomValue = [];
     var mensagemDividida = message.content.split(" ");
     var mensagem_especial = "";
-    const operações =/[*+-/]/g;
+    let flag = [];
 
-    //Module - Dice Rolling//
+    //Modulos//
+    /*Cada modulo corresponde a um conjunto de funções atribuidas a um comando,
+    ou conjunto de comandos.*/
+
+    //Modulo - Rolagem de Dados//
 	if (mensagemDividida[0] === '!roll') {
-	message.delete({timeout: 100});  
+
+        //Deleção da Mensagem que foi enviada//
+        message.delete({timeout: 100});
+
         let Instruções = mensagemDividida[1].split("d");
-        if (Instruções[0] <= 200) {
-         //Se começar com d//
-         if (mensagemDividida[1][0] === 'd') {
-            Instruções.shift();
-            Instruções.unshift("1");
-            console.log(Instruções);
+
+        //Tratamento de Paranteses//
+        let contador = -1;
+        while (Instruções[0][0] == '(') {
+            InstruçõesChar = Instruções[0].split('');
+            console.log(InstruçõesChar);
+            InstruçõesChar.shift();
+            Instruções[0] = InstruçõesChar.join('');
+            console.log(Instruções[0]);
+            ++contador;
+            flag[contador] = 1;
         }
+        if (Instruções[0] <= 200) {
+            //Se começar com d, ele contabiliza um dado//
+            if (mensagemDividida[1][0] === 'd') {
+                Instruções.shift();
+                Instruções.unshift("1");
+            }
+
         //Tratamento de Operações//
         console.log(Instruções[1]);
         Instruções[1] = Instruções[1].replace(/[+]/g, " + ");
         Instruções[1] = Instruções[1].replace(/[-]/g, " - ");
         Instruções[1] = Instruções[1].replace(/[*]/g, " * ");
         Instruções[1] = Instruções[1].replace(/[/]/g, " / ");
+        Instruções[1] = Instruções[1].replace(/[)]/g, " ) ");
+
+        //Após identificar os operadores, split separa eles dos valores de rolagem.//
         let Dado = Instruções[1].split(" ");
         Dado.unshift(Instruções[0]);
-        console.log("Antes da rolagem: " + Dado);
 
         //Execução da Rolagem//
         randomValue = randomGenerating(Dado[1], Dado[0]);
-     
+
         //Soma//
         Sum = arraySum(randomValue);
 
         //Rearranjo//
+        //Adiciona espaços//
         for (i = 1; i < randomValue.length; ++i) {
             randomValue[i] = " " + randomValue[i];
-        }
-        console.log(randomValue);        
-        //Rearrumação de mensagemDividida//
+        }        
         let Valor_Dado = Dado[1];
         Dado[1] = Sum;
         Dado.shift();
-        console.log("Depois da rolagem: " + Dado);
 
-        //União das Arrays//
+        //Montagem de Arrays//
         let União = "";
         for (i = 0; i < Dado.length; ++i) {
             União += Dado[i] + " ";
         }
-        console.log(União);
+        console.log("Hello: " + União);
+        let contador = 0;
+        while (flag[contador] == 1) {
+            ++contador;
+            União = União.split('');
+            União.unshift('(');
+            União = União.join('');
+            }
+        //Resolução das Operações//
         União = eval(União);
-        console.log(União);
 
-        //Central of Operations//
-        //Funções Locais//
+        //Verificação de Operações Adicionais//
+        //Funções//
+
+        //Media//
         function array_media (array, soma_array) {
             media = soma_array/array.length;
             mensagem_especial += "\nA media entre as rolagens é: " + media + ";"; 
         }
 
+        //Comparação//
         function comparação (valor) {
+        
+        //Substituição dos Operadores//
         valor = valor.replace(/>=/g, " >= ");
         valor = valor.replace(/<=/g, " <= ");
         valor = valor.replace(/[>]/g, " > ");
         valor = valor.replace(/[<]/g, " < ");
+        //Após identificar os operadores, split separa eles dos valores de rolagem.//
         let ComparaçõesFinal = valor.split(" ");
         let ComparaçõesFinal2 = "";
         ComparaçõesFinal.shift();
+        //Monta o Array de Comparação//
         ComparaçõesFinal.unshift(União);
         for (j = 0; j < ComparaçõesFinal.length; ++j) {
             ComparaçõesFinal2 += ComparaçõesFinal[j];
         }
         ComparaçõesExpressão = ComparaçõesFinal2;
-        console.log(ComparaçõesFinal2);
         ComparaçõesFinal2 = "if (" + ComparaçõesFinal2 + ') { ComparaçõesFinal2 = "Verdadeira" } else { ComparaçõesFinal2 = "Falsa" }';
+        
+        //Resolve a operação, e devolve "Verdadeira" caso seja verdadeiro e "Falsa" caso seja falso//
+        
         eval(ComparaçõesFinal2);
-        console.log(ComparaçõesFinal2);
         mensagem_especial += "\nA expressão verificada (" + ComparaçõesExpressão + ") é " + ComparaçõesFinal2 + ";"; 
         }
 
-        //Central//
+        //Central de Execução//
         for (i = 2; i < mensagemDividida.length; ++i) {
             console.log("Passou por aqui.");
             if (mensagemDividida[i] === 'media') {
@@ -108,7 +143,31 @@ client.on('message', message => {
     }
 }
 
-    //Funções//
+//Modulo - Conquista//
+if (mensagemDividida[0] === '!conquista') {
+    const Conquista = {
+        index: ["Comum;", "Raro;", "Epico;", "Lendario;", "Extraordinario;", "Lore;"],
+        color_value: ["#fffffe", "#037ffc", "#6d38f6", "#ffc737", "#ff7300", "#bbbcc2"]
+    }
+    let color_index; 
+    for (i = 0; i < 6; ++i) {
+        if (mensagemDividida[1] == Conquista.index[i]) {
+            color_index = Conquista.color_value[i];
+        }   
+    }
+
+    let Aspas = message.content.split('; ');
+    console.log(Aspas);
+    const Embed = new Discord.MessageEmbed()
+        .setColor(color_index)
+        .setTitle(Aspas[1])
+        .setDescription(Aspas[2])
+        .setFooter(Aspas[3] + `\nMestre: ${message.author.username}`);
+
+    message.channel.send(Embed);
+}
+
+    //Funções Auxiliares//
     function randomGenerating(dice, number) {
         let array = [];
             for (number; number > 0; --number) {
