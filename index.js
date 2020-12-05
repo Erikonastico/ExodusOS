83734d038f8585d7f9e27e6c63073bc6bd4a0d9e
Erikonastico <39448705+Erikonastico@users.noreply.github.com>
2020-12-05 18:10:19 -0300

    Update index.js
    Adicionado sistema de rolagem por Vantagem | Desvantagem.

--- index.js
+++ index.js
@@ -6,29 +6,70 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-//Mensage Listening//
+//Command Reciever//
 client.on('message', message => {
-    
+
     //Global Variables//
     var author_id = message.author.id;
     var randomValue = [];
-    var mensagemDividida = message.content.split(" ");
-    var mensagem_especial = "";
-    let flag = [];
+    var splitedMessage = message.content.split(" ");
+    var specialMessage = "";
+    var flag = [];
+    var flag_adv = [];
+    var additionalValue = [];
+    const operations = /[*+-/]/g;
 
-    //Modulos//
-    /*Cada modulo corresponde a um conjunto de funções atribuidas a um comando,
-    ou conjunto de comandos.*/
+    //Facility Variables//
+    command = splitedMessage[0];
 
-    //Modulo - Rolagem de Dados//
-	if (mensagemDividida[0] === '!roll') {
+    //Modules//
 
-        //Deleção da Mensagem que foi enviada//
+    //Dice Roller Module//
+	if (command === '!roll') {
+
         message.delete({timeout: 100});
 
-        let Instruções = mensagemDividida[1].split("d");
+        //Message Content Handler//
+        let Instruções = splitedMessage[1].split("d");
+        Instruções[1] = Instruções[1].replace(/uk/, " ");
+        Instruções[1] = Instruções[1].replace(/k/, " ");
+        let Desvantagem = splitedMessage[1].split("uk");     
+        let Vantagem = splitedMessage[1].split("k");
 
-        //Tratamento de Paranteses//
+        //Advantage | Disadvantage Handler//
+        if (Desvantagem[1] != undefined) {
+            console.log("Desvantagem");
+            Desvantagem[1] = Desvantagem[1].replace(operations," ");
+            DesvantagemImportante = Desvantagem[1];
+            DesvantagemImportante = DesvantagemImportante.split(' ')
+            while (DesvantagemImportante.length > 1) {
+                DesvantagemImportante.pop();
+            }
+            Desvantagem[1] = DesvantagemImportante;
+        }
+        else {
+            if (Vantagem[1] != undefined) { 
+              console.log("Vantagem");
+                Vantagem[1] = Vantagem[1].replace(operations," ");
+                VantagemImportante = Vantagem[1];
+                VantagemImportante = VantagemImportante.split(' ')
+                while (VantagemImportante.length > 1) {
+                    VantagemImportante.pop();
+                }
+                Vantagem[1] = VantagemImportante;
+            }
+        }
+        if (Desvantagem.length > 1) {
+            flag_adv = ["Desvantagem", Desvantagem[1]];
+            console.log(flag_adv);
+        }
+        else {
+            if (Vantagem.length > 1) {
+                flag_adv = ["Vantagem", Vantagem[1]];
+                console.log(flag_adv);
+            }
+        }
+
         let contador = -1;
         while (Instruções[0][0] == '(') {
             InstruçõesChar = Instruções[0].split('');
@@ -39,55 +80,84 @@ client.on('message', message => {
             ++contador;
             flag[contador] = 1;
         }
+
         if (Instruções[0] <= 200) {
-            //Se começar com d, ele contabiliza um dado//
-            if (mensagemDividida[1][0] === 'd') {
+            //If starts with 'D', the system deals with it as like as one dice being rolled//
+            if (splitedMessage[1][0] === 'd') {
                 Instruções.shift();
                 Instruções.unshift("1");
             }
 
-        //Tratamento de Operações//
+        //Operations Handler//
         console.log(Instruções[1]);
         Instruções[1] = Instruções[1].replace(/[+]/g, " + ");
         Instruções[1] = Instruções[1].replace(/[-]/g, " - ");
         Instruções[1] = Instruções[1].replace(/[*]/g, " * ");
         Instruções[1] = Instruções[1].replace(/[/]/g, " / ");
         Instruções[1] = Instruções[1].replace(/[)]/g, " ) ");
-
-        //Após identificar os operadores, split separa eles dos valores de rolagem.//
         let Dado = Instruções[1].split(" ");
         Dado.unshift(Instruções[0]);
 
-        //Execução da Rolagem//
+        //Dice Rolling//
         randomValue = randomGenerating(Dado[1], Dado[0]);
+        console.log(randomValue);
 
-        //Soma//
+        //Advantage//
+        if (flag_adv[0] != "") {
+            console.log("Foi");
+            randomValue = arrayOrganize(randomValue);
+            console.log("Psi: " + randomValue);
+            console.log("Valores: " + randomValue.length + ", " + flag_adv[1])
+            let baseCont = randomValue.length - flag_adv[1];
+            console.log("Ole: " + baseCont);
+            for (i = baseCont; i > 0; --i) {
+                if (flag_adv[0] == "Vantagem") {
+                    additionalValue[i-1] = randomValue.pop();
+                }
+                else {
+                    additionalValue[i-1] = randomValue.shift();
+                }
+            }
+            console.log("Valores Adicionais: " + additionalValue);
+        }
+        console.log("Olá: " + randomValue);
+
+        //Sum//
         Sum = arraySum(randomValue);
 
-        //Rearranjo//
-        //Adiciona espaços//
-        for (i = 1; i < randomValue.length; ++i) {
-            randomValue[i] = " " + randomValue[i];
+        //Dice Array Building//
+        for (i = 0; i < randomValue.length; ++i) {
+            randomValue[i] = ' "' + randomValue[i] + '"';
+        }
+        for (i = 0; i < additionalValue.length; ++i) {
+            if (flag_adv[0] == "Vantagem") {
+                randomValue.push(' ' + additionalValue[i]);
+            }
+            else if (flag_adv[0] == "Desvantagem") {
+                randomValue.unshift(' ' + additionalValue[i]);
+            }
         }        
         let Valor_Dado = Dado[1];
         Dado[1] = Sum;
         Dado.shift();
 
-        //Montagem de Arrays//
-        let União = "";
+        //Array of previous calculated functions//
+        let Junction = "";
         for (i = 0; i < Dado.length; ++i) {
-            União += Dado[i] + " ";
+            if (i != 1) {
+            Junction += Dado[i] + " ";
+            }
         }
-        console.log("Hello: " + União);
+        console.log("Hello: " + Junction);
         let contador = 0;
         while (flag[contador] == 1) {
             ++contador;
-            União = União.split('');
-            União.unshift('(');
-            União = União.join('');
+            Junction = Junction.split('');
+            Junction.unshift('(');
+            Junction = Junction.join('');
             }
-        //Resolução das Operações//
-        União = eval(União);
+
+        Junction = eval(Junction);
 
         //Verificação de Operações Adicionais//
         //Funções//
@@ -95,7 +165,7 @@ client.on('message', message => {
         //Media//
         function array_media (array, soma_array) {
             media = soma_array/array.length;
-            mensagem_especial += "\nA media entre as rolagens é: " + media + ";"; 
+            specialMessage += "\nA media entre as rolagens é: " + media + ";"; 
         }
 
         //Comparação//
@@ -111,7 +181,7 @@ client.on('message', message => {
         let ComparaçõesFinal2 = "";
         ComparaçõesFinal.shift();
         //Monta o Array de Comparação//
-        ComparaçõesFinal.unshift(União);
+        ComparaçõesFinal.unshift(Junction);
         for (j = 0; j < ComparaçõesFinal.length; ++j) {
             ComparaçõesFinal2 += ComparaçõesFinal[j];
         }
@@ -121,22 +191,22 @@ client.on('message', message => {
         //Resolve a operação, e devolve "Verdadeira" caso seja verdadeiro e "Falsa" caso seja falso//
         
         eval(ComparaçõesFinal2);
-        mensagem_especial += "\nA expressão verificada (" + ComparaçõesExpressão + ") é " + ComparaçõesFinal2 + ";"; 
+        specialMessage += "\nA expressão verificada (" + ComparaçõesExpressão + ") é " + ComparaçõesFinal2 + ";"; 
         }
 
         //Central de Execução//
-        for (i = 2; i < mensagemDividida.length; ++i) {
+        for (i = 2; i < splitedMessage.length; ++i) {
             console.log("Passou por aqui.");
-            if (mensagemDividida[i] === 'media') {
-                console.log("Media: " + mensagemDividida.length);
+            if (splitedMessage[i] === 'media') {
+                console.log("Media: " + splitedMessage.length);
                 array_media(randomValue,Sum);
             }
             else {
-                console.log("Comparação: " + mensagemDividida.length);
-                comparação(mensagemDividida[i]);
+                console.log("Comparação: " + splitedMessage.length);
+                comparação(splitedMessage[i]);
             }
         }
-        message.channel.send(`**A rolagem foi concluida, <@${author_id}>` + '.**\n```\nRolagem: ' + Instruções[0] + 'd' + Valor_Dado + ' = [' + Sum + ']\nValor com Operadores (' + mensagemDividida[1] + ') = '+ União + '\nValores Individuais: [' + randomValue + ']\nOperações Especiais: ' + mensagem_especial + '```');
+        message.channel.send(`**A rolagem foi concluida, <@${author_id}>` + '.**\n```bash\nRolagem: ' + Instruções[0] + 'd' + Valor_Dado + ' = [' + Sum + ']\nValor com Operadores (' + splitedMessage[1] + ') = '+ Junction + '\nValores Individuais: [' + randomValue + ' ]\nOperações Especiais: ' + specialMessage + '```');
     }
     else {
         message.channel.send("*Na rolagem normal, só aceitamos rolar de 1 à 200 dados. Tente novamente.*")
@@ -144,14 +214,14 @@ client.on('message', message => {
 }
 
 //Modulo - Conquista//
-if (mensagemDividida[0] === '!conquista') {
+if (command === '!conquista') {
     const Conquista = {
         index: ["Comum;", "Raro;", "Epico;", "Lendario;", "Extraordinario;", "Lore;"],
         color_value: ["#fffffe", "#037ffc", "#6d38f6", "#ffc737", "#ff7300", "#bbbcc2"]
     }
     let color_index; 
     for (i = 0; i < 6; ++i) {
-        if (mensagemDividida[1] == Conquista.index[i]) {
+        if (splitedMessage[1] == Conquista.index[i]) {
             color_index = Conquista.color_value[i];
         }   
     }
@@ -181,6 +251,20 @@ if (mensagemDividida[0] === '!conquista') {
             sum += value[i-1];    
         }
         return sum;
+    }
+    function arrayOrganize(value) {
+        let medium = 0;
+        for (i = 0; i < value.length; ++i) {
+            for (j = 0; j < value.length; ++j) {
+                if (value[i] > value[j]) {
+                    medium = Number(value[i]);
+                    value[i] = Number(value[j]);
+                    value[j] = Number(medium);
+                }
+            }
+        }
+        console.log(Number(value));
+        return value;
     }
 })
 client.login();
