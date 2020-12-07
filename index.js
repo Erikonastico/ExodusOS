582b63e14a7cb4b039ca4b40baa028472b23c978
Erikonastico <39448705+Erikonastico@users.noreply.github.com>
2020-12-07 13:49:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -362,4 +362,35 @@ if (command === '!conquista') {
     message.channel.send(Embed);
 }
 }
+
+    //Funções Auxiliares//
+    function randomGenerating(dice, number) {
+        let array = [];
+            for (number; number > 0; --number) {
+                array[number-1] = Number(Math.floor((Math.random() * dice) + 1));
+            }
+        return array;
+    }
+    function arraySum(value) {
+        let sum = 0;
+        for (i = value.length; i > 0; --i) {
+            sum += value[i-1];    
+        }
+        return sum;
+    }
+    function arrayOrganize(value) {
+        let medium = 0;
+        for (i = 0; i < value.length; ++i) {
+            for (j = 0; j < value.length; ++j) {
+                if (value[i] > value[j]) {
+                    medium = Number(value[i]);
+                    value[i] = Number(value[j]);
+                    value[j] = Number(medium);
+                }
+            }
+        }
+        console.log(Number(value));
+        return value;
+    }
+})
 client.login();
